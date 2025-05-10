c67ab460e30fa38f28c6ad7e42ee3b869f89d396
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2025-05-10 17:16:55 -0300

    feat: created form input

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,6 +2,8 @@
 
 import styled from "styled-components";
 
+import Input from "@/components/form/Input";
+
 const Container = styled.div`
     padding: 0;
     margin: 0;
@@ -12,12 +14,21 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
 `
 
 export default function LoginPage() {
     return(
         <Container>
             <img src="logo.png" alt="" />
+            <Form>
+                <Input placeholder="Usuario"/>
+            </Form>
         </Container>
     )
 }
