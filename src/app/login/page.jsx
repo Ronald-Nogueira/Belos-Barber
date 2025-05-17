ac5ca069673901c4aac7bcc1558a3a5508b8cbf6
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2025-05-17 18:43:07 -0300

    feat: created logic for login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,9 @@
-"use client"
+"use client";
 
 import styled from "styled-components";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 import Input from "@/components/form/Input";
 import H1 from "@/components/typography/H1";
@@ -8,49 +11,85 @@ import H3 from "@/components/typography/H3";
 import Button from "@/components/form/Button";
 
 const Container = styled.div`
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    width: 100%;
-    height: 100%;
-    background-color: black;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `
 
 const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
 `
 
 const StyleH1 = styled(H1)`
-    margin-right: 170px;
+  margin-right: 170px;
 `
 
 const TextContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-right: 100px;
+  display: flex;
+  flex-direction: column;
+  margin-right: 100px;
+`
+
+const StyledInput = styled(Input)`
+  width: 300px;
 `
 
 export default function LoginPage() {
-    return(
-        <Container>
-            <img src="logo.png" alt="" />
-            <Form>
-                <StyleH1>Faça Login!</StyleH1>
-                <Input placeholder="Usuário"/>
-                <Input placeholder="Senha"/>
-                <TextContainer>
-                    <H3>Ainda não é cadastrado?</H3>
-                    <H3 color="#FF7000">Cadastre-se</H3>
-                </TextContainer>
-                <Button>Login</Button>
-            </Form>
-        </Container>
-    )
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [erro, setErro] = useState("")
+
+  const router = useRouter()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await axios.post("http://localhost:3001/auth/login", {
+        email,
+        senha,
+      })
+      router.push('/')
+    } catch (error) {
+      console.error("Erro no login:", error)
+      setErro("Email ou senha inválidos")
+    }
+  }
+
+  return (
+    <Container>
+      <img src="logo.png" alt="Logo" />
+      <Form onSubmit={handleSubmit}>
+        <StyleH1>Faça Login!</StyleH1>
+        <StyledInput
+          type="email"
+          placeholder="Usuário"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <StyledInput
+          type="password"
+          placeholder="Senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+        {erro && <H3 color="red">{erro}</H3>}
+        <TextContainer>
+          <H3>Ainda não é cadastrado?</H3>
+          <H3 color="#FF7000">Cadastre-se</H3>
+        </TextContainer>
+        <Button type="submit">Login</Button>
+      </Form>
+    </Container>
+  );
 }
